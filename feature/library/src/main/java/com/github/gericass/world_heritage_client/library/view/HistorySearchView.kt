9df49c9e94077b49d3a93ef1cd059c45d86a0c15
gericass <bonjovi1926@yahoo.co.jp>
2019-11-28 18:52:25 +0900

    wip

--- feature/library/src/main/java/com/github/gericass/world_heritage_client/library/view/HistorySearchView.kt
+++ feature/library/src/main/java/com/github/gericass/world_heritage_client/library/view/HistorySearchView.kt
@@ -3,10 +3,12 @@ package com.github.gericass.world_heritage_client.library.view
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.airbnb.epoxy.ModelView
 import com.airbnb.paris.annotations.Styleable
 import com.github.gericass.world_heritage_client.library.R
+import kotlinx.android.synthetic.main.library_view_history_search.view.*
 
 @Styleable
 @ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
@@ -14,25 +16,36 @@ class HistorySearchView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : MotionLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     init {
         inflate(context, R.layout.library_view_history_search, this)
         val color = ContextCompat.getColor(getContext(), R.color.common_gray_background)
         setBackgroundColor(color)
-        loadLayoutDescription(R.xml.library_motion_cancel)
-        //setOnClickListener {
-        //    if (currentState == startState) {
-        //        Timber.d(currentState.toString())
-        //        transitionToEnd()
-        //        updateState()
-        //        requestLayout()
-        //        Timber.d(currentState.toString())
-        //    } else {
-        //        transitionToStart()
-        //    }
-//
-        //}
+        search_edit_text.setOnFocusChangeListener { v, focused ->
+            if (focused) v.callOnClick()
+        }
+        search_motion_layout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, state: Int) {
+                if (state == search_motion_layout.startState) {
+                    search_edit_text.clearFocus()
+                }
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+        })
+
+
     }
 
     companion object {
